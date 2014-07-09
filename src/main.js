a84ff711d1b534749ba1946f45c7d18e3ec920ff
Justin McNally <justin@kohactive.com>
2014-07-09 17:41:45 -0500

    minor tweaks

--- src/main.js
+++ src/main.js
@@ -3,18 +3,23 @@
 
 
   xtag.register('x-amazing-rainbow-widget', {
+    content: '<h1>An amazing widget</h1>',
     lifecycle: {
   		created: function(){
         this.locals = {
           count: 0,
           colors: ['red', 'green', 'blue', 'yellow'],
-        }; 
+        };
+        var inner = this.innerHTML.replace(/\W/g, "");
+        if (inner.length <= 0) {
+          this.innerHTML = "I am the default text";
+        }
   		}
   	},
   	events: {
       'click:delegate(x-amazing-rainbow-widget)': function(){
         if (this.locals.count >= this.locals.colors.length) {
-          this.locals.count = 0
+          this.locals.count = 0;
         }
         this.color = this.locals.colors[this.locals.count];
         this.locals.count++;
